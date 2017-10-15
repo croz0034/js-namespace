9a1d97761dd0977ca6f1d16ef1c52bd31f248789
croz0034 <32398078+croz0034@users.noreply.github.com>
2017-10-14 22:41:39 -0400

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -3,7 +3,7 @@ var CROZ0034 = {
         let pandora = document.getElementById("boxes");
         let cardBoard = document.createElement("div");
         cardBoard.className = "box";
-        cardBoard.textContent= "croz0034y";
+        cardBoard.textContent= "croz0034";
         pandora.appendChild(cardBoard);
         cardBoard.addEventListener("click", curiosity);
         cardBoard.addEventListener("mouseover", highlight);
